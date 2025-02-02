583bb32caa215dac71d3c78d799a52e088a92c9c
Thomas Vy <vythomas97@gmail.com>
2025-02-02 09:12:37 +0000

    Adds the latest nvim

--- .scripts/install/install_nvim.sh
+++ .scripts/install/install_nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
