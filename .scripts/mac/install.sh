4b89913d5f390e7901205fd9c73e3bf87760ed36
Thomas Vy <vythomas97@gmail.com>
2025-10-02 15:22:02 -0600

    adds scripts to mac and linux to it's own folder

---
+++ .scripts/mac/install.sh
@@ -0,0 +1,15 @@
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install ripgrep
+brew install fzf 
+brew install zoxide
+brew install cmake
+brew install neovim 
+brew install node
+brew install python
+brew install go
+brew install bottom
+brew install bat
+brew install fd
+
+
