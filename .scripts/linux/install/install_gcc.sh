696df185dac83b18bfc7202e343f73eb19a3d9ac
Thomas Vy <vythomas97@gmail.com>
2026-01-11 15:41:18 -0700

    removes vcpkg

--- .scripts/linux/install/install_gcc.sh
+++ .scripts/linux/install/install_gcc.sh
@@ -3,8 +3,3 @@
 
 sudo apt install ninja-build curl zip unzip tar pkg-config clang-format cmake gcc g++ -y
 
-git clone https://github.com/microsoft/vcpkg.git ~/vcpkg
-cd ~/vcpkg
-
-./bootstrap-vcpkg.sh
-
